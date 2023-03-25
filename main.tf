528310417a28356407e92768840656f9d90b0a8f
sumayya-005 <107997976+sumayya-005@users.noreply.github.com>
2023-03-25 11:32:48 +0530

    updating ./tf-module-vpc

--- main.tf
+++ main.tf
@@ -8,5 +8,6 @@ resource "aws_vpc" "main" {
 module "additional_cidr_block" {
   for_each = var.vpc
   source   = "./additional_vpc_cidr"
-  additonal_cidr_block = each.value.additonal_cidr_block
+  additional_cidr_block = each.value.additional_cidr_block
+  vpc_id = var.vpc_id
 }
