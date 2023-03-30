cd4d886a00e8758a2b52df584b5c910b4925da4b
sumayya-005 <107997976+sumayya-005@users.noreply.github.com>
2023-03-30 10:32:08 +0530

    updating ./tf-module-vpc

--- locals.tf
+++ locals.tf
@@ -4,6 +4,12 @@ locals {
     ENV   = var.env
     PROJECT = "roboshop"
   }
+  vpc_peering_tags = {
+    Name  = "${var.env}-vpc-to-mnagement-vpc"
+    ENV   = var.env
+    PROJECT = "roboshop"
+
+  }
   vpc_ids = [for k,v in aws_vpc.main : v.id]
 }
 
