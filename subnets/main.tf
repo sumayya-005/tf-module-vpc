66c927297bd80b4dd7548838c1ad7ddbd9516db9
sumayya-005 <107997976+sumayya-005@users.noreply.github.com>
2023-03-31 16:07:42 +0530

    updating ./tf-module-vpc

--- subnets/main.tf
+++ subnets/main.tf
@@ -6,7 +6,7 @@ module "lm-subnets" {
   env         = var.env
   name        =each.value.name
   subnet_availability_zones = var.subnet_availability_zones
-  route_table_id    = lookup(aws_route_table.route_table,each.value.name,null)
+  route_table_id    = lookup(lookup(aws_route_table.aws_route_table,each.value.name,null) "id" ,null)
 }
 
 resource "aws_route_table" "route_table" {
@@ -20,6 +20,12 @@ resource "aws_route_table" "route_table" {
   }
 }
 
-output "aws_route_table" {
-  value = module.lm-subnets
+#output "aws_route_table" {
+#  value = module.lm-subnets
+#}
+
+#to get the subnet id.
+
+output "route-tables" {
+  value =aws_route_table.aws_route_table
 }
