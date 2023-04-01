73ebe4d60f398a13ab6a206a3277dcfc9fc18e41
sumayya-005 <107997976+sumayya-005@users.noreply.github.com>
2023-04-01 21:36:50 +0530

    updating ./tf-module-vpc

--- gateway.tf
+++ gateway.tf
@@ -1,2 +1,11 @@
-resource "aws_internet_gateway" "gw" {}
-vpc_id = aws_vpc.main.id
+resource "aws_internet_gateway" "gw" {
+  count = length (local.vpc_ids)
+  vpc_id = aws_vpc.main.id
+
+  tags ={
+    Name = "${var.env}-igw"
+  }
+}
+
+
+
