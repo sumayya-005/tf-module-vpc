5ccd30e675482a908ad6fe3565741613681be068
sumayya-005 <107997976+sumayya-005@users.noreply.github.com>
2023-04-06 11:25:30 +0530

    updating ./tf-module-vpc

--- subnets.tf
+++ subnets.tf
@@ -24,13 +24,10 @@ module "private_subnets" {
 
 
 output "public_subnets"{
-  value = module.public_subnets
+  value = module.public_subnets.subnets
 }
 
 output "private_subnets"{
-  value = module.private_subnets
+  value = module.private_subnets.subnets
 }
 
-output "all_private_subnets" {
-  value = [ for k,v in module.private_subnets :v.id ]
-}
