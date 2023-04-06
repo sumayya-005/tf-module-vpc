f86e00e78e8d35e000edf212490bf86d335c5397
sumayya-005 <107997976+sumayya-005@users.noreply.github.com>
2023-04-06 16:48:59 +0530

    updating ./tf-module-vpc

--- subnets.tf
+++ subnets.tf
@@ -23,14 +23,17 @@ module "private_subnets" {
 }
 
 
-output "public_subnets"{
-  value = module.public_subnets
-}
+
 
 output "private_subnets"{
   value = module.private_subnets
 }
 
-output "all_private_subnets" {
-  value = [for i,j in tomap ([for k,v in module.private_subnets : v.subnets]) : j.subnets]
+output "public_subnets"{
+  value = module.public_subnets
 }
+
+
+#output "all_private_subnets" {
+#  value = [for i,j in tomap ([for k,v in module.private_subnets : v.subnets]) : j.subnets]
+#}
