edc99a65895a2c9ec9275fd2fc05c30549de2439
sumayya-005 <107997976+sumayya-005@users.noreply.github.com>
2023-04-06 10:30:11 +0530

    updating ./tf-module-vpc

--- subnets/lm-subnets/main.tf
+++ subnets/lm-subnets/main.tf
@@ -10,7 +10,7 @@ resource "aws_route" "route-to_default_management_route_table" {
   for_each       = var.vpc
   route_table_id = lookup(var.management_vpc,"route_table",null )
   destination_cidr_block = each.value.cidr_block
-  vpc_peering_connection_id = aws_vpc_peering_connection.management-vpc-to-env-vpc.*id[0]
+  vpc_peering_connection_id = aws_vpc_peering_connection.management-vpc-to-env-vpc. * .id[0]
 }
 
 output "subnets"{
