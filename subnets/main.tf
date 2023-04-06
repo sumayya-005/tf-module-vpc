0cbc70aadefd4d8850217a23d13dd871e4a0b6c6
sumayya-005 <107997976+sumayya-005@users.noreply.github.com>
2023-04-06 16:12:37 +0530

    updating ./tf-module-vpc

--- subnets/main.tf
+++ subnets/main.tf
@@ -46,7 +46,7 @@ resource "aws_route" "peering_connection_route" {
 #}
 
 output "subnets" {
-  value = [ for i,j in module.lm-subnets: concat(j.subnets[0])]
+  value = flatten([ for i,j in module.lm-subnets: concat(j.subnets)])
 }
 
 resource "null_resource" "test" {
