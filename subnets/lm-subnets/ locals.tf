4de3a4763f7a653b9ef7dfecb044555dedf54157
sumayya-005 <107997976+sumayya-005@users.noreply.github.com>
2023-04-06 10:02:26 +0530

    updating ./tf-module-vpc

--- subnets/lm-subnets/ locals.tf
+++ subnets/lm-subnets/ locals.tf
@@ -1,7 +1,13 @@
 locals {
+  vpc_tags ={
+    Name  = "${var.env}-vpc"
+    ENV   = var.env
+    PROJECT ="roboshop"
+  }
   subnet_tags  = {
-    Name   = "${var.name}-subnet"
+    Name   = "${var.name}-vpc-to management-vpc"
     ENV    = var.env
     PROJECT = "roboshop"
   }
+  vpc_ids = [for k,v in aws_vpc.main :v.id]
 }
