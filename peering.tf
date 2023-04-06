ee5e35e334c463898675d085b7c0605f3496fcf9
sumayya-005 <107997976+sumayya-005@users.noreply.github.com>
2023-04-06 10:18:53 +0530

    updating ./tf-module-vpc

--- peering.tf
+++ peering.tf
@@ -1,5 +1,5 @@
 resource "aws_vpc_peering_connection" "management-vpc-to-env-vpc" {
-  count         = lenght (local.vpc_ids)
+  count         = length (local.vpc_ids)
   peer_vpc_id   = lookup(var.management_vpc,"vpc_id", null)
   vpc_id        = element(local.vpc_ids,count.index)
   auto_accept   = true
