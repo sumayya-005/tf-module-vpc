76dae704d4d5cdc04675c57db3bdb5bc6374a6a0
sumayya-005 <107997976+sumayya-005@users.noreply.github.com>
2023-04-10 13:19:21 +0530

    updating ./tf-module-vpc

--- locals.tf
+++ locals.tf
@@ -1,14 +1,14 @@
-locals {
-  vpc_tags  = {
-    Name  = "${var.env}-vpc"
-    ENV   = var.env
-    PROJECT = "roboshop"
-  }
-  vpc_peering_tags = {
-    Name  = "${var.env}-vpc-to-management-vpc"
-    ENV   = var.env
-    PROJECT = "roboshop"
-
-  }
-}
+#locals {
+#  vpc_tags  = {
+#    Name  = "${var.env}-vpc"
+#    ENV   = var.env
+#    PROJECT = "roboshop"
+#  }
+#  vpc_peering_tags = {
+#    Name  = "${var.env}-vpc-to-management-vpc"
+#    ENV   = var.env
+#    PROJECT = "roboshop"
+#
+#  }
+#}
 
--- vars.tf
+++ vars.tf
@@ -2,6 +2,6 @@ variable "env" {}
 variable "cidr_block" {}
 variable "additional_cidr_block" {}
 variable "private_subnets" {}
-#variable "public_subnets" {}
+variable "public_subnets" {}
 variable "subnet_availability_zones" {}
 variable "management_vpc" {}
