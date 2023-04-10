7365046c1e058785293c5f9423b177f241ea2d8d
sumayya-005 <107997976+sumayya-005@users.noreply.github.com>
2023-04-10 16:19:57 +0530

    updating ./tf-module-vpc

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,7 @@ output "private_subnets" {
 #output "public_subnets"{
 #value = module.public_subnets
 #}
+
+output "test" {
+ value = lookup(lookup(module.public_subnets, "public", null), "subnets", null)[0].id
+}
