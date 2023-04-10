068a0abe2e25fa4d8925957b95d06878bb9ee188
sumayya-005 <107997976+sumayya-005@users.noreply.github.com>
2023-04-10 11:57:42 +0530

    updating ./tf-module-vpc

--- vars.tf
+++ vars.tf
@@ -1,3 +1,5 @@
- variable "vpc" {}
- variable "env" {}
-variable "management_vpc" {}
+variable "env" {}
+variable "cidr_block" {}
+variable "additional_cidr_block" {}
+variable "private_subnets" {}
+variable "public_subnets" {}
