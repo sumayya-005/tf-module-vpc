7908ac1c268162ad2c7290bc1e5c65b0b157489e
sumayya-005 <107997976+sumayya-005@users.noreply.github.com>
2023-04-10 16:41:01 +0530

    updating ./tf-module-vpc

--- peering.tf
+++ peering.tf
@@ -1,14 +1,19 @@
-#resource "aws_vpc_peering_connection" "management-vpc-to-env-vpc" {
-#  count         = length (local.vpc_ids)
-#  peer_vpc_id   = lookup(var.management_vpc,"vpc_id", null)
-#  vpc_id        = element(local.vpc_ids,count.index)
-#  auto_accept   = true
-#  tags          =local.vpc_peering_tags
-#}
-#
-#resource "aws_route" "route_to_default_management_route_table" {
-#  for_each               = var.vpc
-#  route_table_id         = lookup(var.management_vpc,"route_table", null)
-#  destination_cidr_block = each.value.cidr_block
-#  vpc_peering_connection_id = aws_vpc_peering_connection.management-vpc-to-env-vpc.*.id[0]
-#}
+resource "aws_vpc_peering_connection" "management-vpc-to-env-vpc" {
+  peer_vpc_id = aws_vpc.main.id
+  vpc_id      = lookup(var.management_vpc, "vpc_id", null)
+  auto_accept = true
+  tags        = local.vpc_peering_tags
+}
+
+resource "aws_route" "route_from_default_management_route_table" {
+  count                     = length(local.all_route_tables)
+  route_table_id            = element(local.all_route_tables, count.index)
+  destination_cidr_block    = lookup(var.management_vpc, "vpc_cidr", null)
+  vpc_peering_connection_id = aws_vpc_peering_connection.management-vpc-to-env-vpc.id
+}
+
+resource "aws_route" "route_to_default_management_route_table" {
+  route_table_id            = lookup(var.management_vpc, "route_table", null)
+  destination_cidr_block    = var.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.management-vpc-to-env-vpc.id
+}
--- subnets.tf
+++ subnets.tf
@@ -6,7 +6,7 @@ module "private_subnets" {
   env                       = var.env
   name                      = each.value.name
   subnet_availability_zones = var.subnet_availability_zones
-   //internet_gateway_id      = aws_nat_gateway.ngw.id
+  gateway_id                = aws_nat_gateway.ngw.id
 }
 
 module "public_subnets" {
@@ -17,5 +17,5 @@ module "public_subnets" {
   env                       = var.env
   name                      = each.value.name
   subnet_availability_zones = var.subnet_availability_zones
-  //gateway_id                = aws_internet_gateway.igw.id
+  gateway_id                = aws_internet_gateway.igw.id
 }
