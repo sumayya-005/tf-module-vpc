18255011d0e9e3a6a12c5e9520f6b59dd6b9a0c6
sumayya-005 <107997976+sumayya-005@users.noreply.github.com>
2023-04-13 14:44:37 +0530

    updating ./tf-module-vpc

--- peering.tf
+++ peering.tf
@@ -5,12 +5,12 @@ resource "aws_vpc_peering_connection" "management-vpc-to-env-vpc" {
   tags        = local.vpc_peering_tags
 }
 
-#resource "aws_route" "route_from_default_management_route_table" {
-#  count                     = length(local.all_route_tables)
-#  route_table_id            = element(local.all_route_tables, count.index)
-#  destination_cidr_block    = lookup(var.management_vpc, "vpc_cidr", null)
-#  vpc_peering_connection_id = aws_vpc_peering_connection.management-vpc-to-env-vpc.id
-#}
+resource "aws_route" "route_from_default_management_route_table" {
+  count                     = length(local.all_route_tables)
+  route_table_id            = element(local.all_route_tables, count.index)
+  destination_cidr_block    = lookup(var.management_vpc, "vpc_cidr", null)
+  vpc_peering_connection_id = aws_vpc_peering_connection.management-vpc-to-env-vpc.id
+}
 
 #resource "aws_route" "route_to_default_management_route_table" {
 #  route_table_id            = lookup(var.management_vpc, "route_table", null)
