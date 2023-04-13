3a217fb139eb4f1640dfe655f40d25ec2b3843e5
sumayya-005 <107997976+sumayya-005@users.noreply.github.com>
2023-04-13 16:19:11 +0530

    updating ./tf-module-vpc

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,10 @@
-#output "private_subnets" {
-# value = module.private_subnets
-#}
-#
-#output "public_subnets" {
-# value = module.public_subnets
-#}
+output "private_subnets" {
+ value = module.private_subnets
+}
+
+output "public_subnets" {
+ value = module.public_subnets
+}
 
 output "all_route_tables" {
  value = local.all_route_tables
@@ -15,11 +15,11 @@ output "test" {
  value = lookup(lookup(module.public_subnets, "public", null), "subnets", null)[0].id
 }
 
-#output "vpc_id" {
-# value = aws_vpc.main.id
-#}
-#
-#output "vpc_cidr" {
-# value = aws_vpc.main.cidr_block
-#}
+output "vpc_id" {
+ value = aws_vpc.main.id
+}
+
+output "vpc_cidr" {
+ value = aws_vpc.main.cidr_block
+}
 
