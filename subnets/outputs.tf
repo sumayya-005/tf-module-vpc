4e8ffc4ed2b58f7eb8c7e9a2482afef44a2b6ebb
sumayya-005 <107997976+sumayya-005@users.noreply.github.com>
2023-04-13 11:59:30 +0530

    updating ./tf-module-vpc

--- subnets/outputs.tf
+++ subnets/outputs.tf
@@ -1,14 +1,14 @@
-#locals {
-#  subnets_list = flatten([ for i,j in module.lm-subnets: j.subnets])
+##locals {
+##  subnets_list = flatten([ for i,j in module.lm-subnets: j.subnets])
+##}
+##output "subnets_list" {
+##  value = local.subnets_list[*].id
+##}
+#
+#
+#output "rt" {
+#  value = [for i,j in aws_route_table.aws_route_table: j.id]
 #}
-#output "subnets_list" {
-#  value = local.subnets_list[*].id
-#}
-
-
-output "rt" {
-  value = [for i,j in aws_route_table.aws_route_table: j.id]
-}
 
 output "subnets" {
   value = aws_subnet.main
