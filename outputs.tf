798e399d727a7b0811eb756d83746bb965f282e3
sumayya-005 <107997976+sumayya-005@users.noreply.github.com>
2023-04-20 12:38:02 +0530

    updating ./tf-module-vpc

--- outputs.tf
+++ outputs.tf
@@ -15,10 +15,10 @@ output "test" {
  value = lookup(lookup(module.public_subnets, "public", null), "subnets", null)[0].id
 }
 
-#output "vpc_id" {
-# value = aws_vpc.main.id
-#}
-#
+output "vpc_id" {
+ value = aws_vpc.main.id
+}
+
 #output "vpc_cidr" {
 # value = aws_vpc.main.cidr_block
 #}
