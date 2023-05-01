b1b491a9319b40c5c41cbac199525533b321e865
sumayya-005 <107997976+sumayya-005@users.noreply.github.com>
2023-05-01 11:31:00 +0530

    updating ./tf-module-vpc

--- outputs.tf
+++ outputs.tf
@@ -18,8 +18,8 @@ output "test" {
 output "vpc_id" {
  value = aws_vpc.main.id
 }
-#
-#output "vpc_cidr" {
-# value = aws_vpc.main.cidr_block
-#}
+
+output "vpc_cidr" {
+ value = aws_vpc.main.cidr_block
+}
 
